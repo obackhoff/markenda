9ac79e7c8b8a3a5da4cb076b384c644bd031f070
Omar Backhoff <omar.backhoff@expleogroup.com>
2020-03-02 21:09:59 +0100

    corrected xfce4-terminal

--- markenda-core.sh
+++ markenda-core.sh
@@ -11,7 +11,7 @@ AGENDA="$WF/agenda.gcal"
 EDITOR="typora "
 EDITOR_TERM="nvim"
 TERM="gnome-terminal --wait -- bash -c"
-# TERM="xfce4-terminal -e"
+# TERM="xfce4-terminal --disable-server -e"
 
 function show_upcoming {
     echo "Upcoming (up to 30 days): "
